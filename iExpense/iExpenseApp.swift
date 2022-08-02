7f6b3e728d0db7e159984cc737702824ab2904fe
Saurabh Jamadagni <saurabhjamadagni2002@gmail.com>
2022-08-02 15:53:41 +0530

    initial commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Admin on 02/08/22.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
